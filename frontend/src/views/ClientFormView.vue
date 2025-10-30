6cecf1f17b4bcef69d709f8e90d6abe0f1a883bd
Sofia Rohrer <sofia@digitalorganizing.ch>
2025-10-30 17:02:02 +0100

    EOD commit -> icons not yet working

--- frontend/src/views/ClientFormView.vue
+++ frontend/src/views/ClientFormView.vue
@@ -2,6 +2,8 @@
 import { sureApiGetQuestionnaire, type QuestionnaireSchema } from '@/client';
 import ClientSection from '@/components/ClientSection.vue';
 import { onMounted, ref } from 'vue';
+import Button from 'primevue/button';
+
 
 const formStructure = ref <QuestionnaireSchema|null>(null)
 const formIndex = ref<number>(0)
@@ -17,7 +19,18 @@ onMounted(async ()=> {formStructure.value = (await sureApiGetQuestionnaire({path
     <h1>Client Form</h1>
     <ProgressBar :total="formStructure?.sections.length" :value="formIndex + 1" />
     <ClientSection :section="formStructure?.sections[formIndex]!" />
-    <button @click="formIndex--">Previous</button>
-    <button @click="formIndex++">Next</button>
+    <div>
+        <Button label="Previous" icon="IconRightArrow" icon-pos="right" @click="formIndex--" rounded/>
+        <Button @click="formIndex++" rounded >Next</Button>
+    </div>
   </div>
 </template>
+
+<style scoped>
+    div {
+        margin: 20px;
+    }
+    button {
+        margin: 10px;
+    }
+</style>

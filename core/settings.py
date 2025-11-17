26c51bd02c510d5bacccfbd7e2d581aefbb5fe37
Sandro Covo <sandro@sandrocovo.ch>
2025-11-18 00:05:11 +0100

    Build with DEBUG=False

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,7 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env.str("SECRET_KEY", default="secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=True)
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 

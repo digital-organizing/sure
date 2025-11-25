76b937f73dec790c0d35201e873141ac139675cf
Sandro Covo <sandro@sandrocovo.ch>
2025-11-25 10:54:01 +0000

    Implement texts with tranlationsi in backend

---
+++ frontend/src/composables/useTexts.ts
@@ -0,0 +1,100 @@
+import { textsApiListLanguages, textsApiListTexts } from '@/client'
+import { createGlobalState } from '@vueuse/core'
+import { computed, ref } from 'vue'
+
+export const useTexts = createGlobalState(() => {
+  const texts = ref<Record<string, string>>({})
+  const availableLanguages = ref<[string, string][]>([])
+
+  const loadingAvailableLanguagesPromise = ref<Promise<void> | null>(null)
+  const loadingPromise = ref<Promise<void> | null>(null)
+
+  const language = ref<string | null>(null)
+  const rightToLeft = ref<boolean>(false)
+
+  async function loadAvailableLanguages() {
+    loadingAvailableLanguagesPromise.value = textsApiListLanguages().then((response) => {
+      if (!response.data) return
+      availableLanguages.value = response.data
+    })
+    await loadingAvailableLanguagesPromise.value
+  }
+
+  loadAvailableLanguages()
+
+  async function setLanguage(lang: string) {
+    if (language.value === lang) return
+    if (loadingAvailableLanguagesPromise.value) {
+      await loadingAvailableLanguagesPromise.value
+    }
+    if (loadingPromise.value) {
+      await loadingPromise.value
+      if (language.value === lang) return
+    }
+    if (!availableLanguages.value.find(([code, _]) => code === lang)) {
+      console.warn(`Language ${lang} is not available.`)
+      return
+    }
+    loadingPromise.value = textsApiListTexts({ query: { lang } }).then((response) => {
+      if (!response.data) return
+      texts.value = response.data.texts
+      rightToLeft.value = response.data.right_to_left
+      language.value = response.data.language!
+    })
+
+    await loadingPromise.value
+  }
+
+  async function getLanguage() {
+    return language.value
+  }
+
+  function getText(slug: string) {
+    if (language.value === null) {
+      setLanguage('en')
+    }
+    return computed(() => {
+      return texts.value[slug] || slug
+    })
+  }
+
+  function formatText(slug: string, args: Record<string, string>[]) {
+    if (language.value === null) {
+      setLanguage('en')
+    }
+    return computed(() => {
+      if (!texts.value[slug]) return slug
+
+      let text = texts.value[slug]
+      args.forEach(({ key, value }) => {
+        const regex = new RegExp(`{${key}}`, 'g')
+        text = text.replace(regex, value)
+      })
+      return text
+    })
+  }
+
+  async function getAvailableLanguages() {
+    if (loadingAvailableLanguagesPromise.value) {
+      await loadingAvailableLanguagesPromise.value
+    }
+    return availableLanguages.value
+  }
+
+  async function isRightToLeft() {
+    if (loadingPromise.value) {
+      await loadingPromise.value
+    }
+    return rightToLeft.value
+  }
+
+  return {
+    texts,
+    setLanguage,
+    getLanguage,
+    getText,
+    getAvailableLanguages,
+    isRightToLeft,
+    formatText,
+  }
+})

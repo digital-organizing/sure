6844f42a87be477728e133ca38be00bbce7cb040
Sandro Covo <sandro@sandrocovo.ch>
2026-01-13 12:16:07 +0000

    Filter location based on list of ids

--- frontend/src/stores/filters.ts
+++ frontend/src/stores/filters.ts
@@ -6,16 +6,16 @@ import { FilterMatchMode, FilterOperator } from '@primevue/core/api'
 
 export const useFilterStore = defineStore('filters', () => {
   const filters = ref<DataTableFilterMeta>({
-    search: { value: null, matchMode: 'contains' },
-    case: { value: null, matchMode: 'contains' },
-    client_id: { value: null, matchMode: 'contains' },
-    external_id: { value: null, matchMode: 'contains' },
+    search: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    case: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    client_id: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    external_id: { value: null, matchMode: FilterMatchMode.CONTAINS },
     tags: {
       operator: FilterOperator.OR,
       constraints: [{ value: null, matchMode: FilterMatchMode.CONTAINS }],
     },
-    location: { value: null, matchMode: 'equals' },
-    status: { value: null, matchMode: 'in' },
+    location: { value: null, matchMode: FilterMatchMode.IN },
+    status: { value: null, matchMode: FilterMatchMode.IN },
     last_modified_at: {
       operator: FilterOperator.AND,
       constraints: [{ value: null, matchMode: FilterMatchMode.DATE_AFTER }],

981aa7ab0939edf0ca86c9394eb74e6bbe025e0f
Sandro Covo <sandro@sandrocovo.ch>
2026-05-26 21:21:52 +0000

    Supress typing issues

--- sure/cases.py
+++ sure/cases.py
@@ -266,24 +266,26 @@ def get_client_answers_export(visit: Visit):
                 }
             else:
                 # Use prefetched options from the question
-                options = {opt.code: opt for opt in question.options.all()}  # type: ignore
+                options = {opt.code: opt for opt in question.options.all()}
 
                 answers_en = []
                 for code, text in zip(answer.choices, answer.texts):
                     option = options.get(str(code))
                     if not option:
                         text_en = text
-                    elif option.choices_en:
+                    elif option.choices_en: # type: ignore
                         original_choices = getattr(option, f"choices_{lang}", [])
                         try:
                             idx = original_choices.index(text)
-                            text_en = option.choices_en[idx]
+                            text_en = option.choices_en[idx] # type: ignore
                         except ValueError:
                             text_en = text
                     elif option.allow_text:
-                        text_en = option.text_en + ": " + text if option.text_en else text
+                        text_en = (
+                            option.text_en + ": " + text if option.text_en else text # type: ignore
+                        )
                     else:
-                        text_en = option.text_en if option.text_en else text
+                        text_en = option.text_en if option.text_en else text # type: ignore
                     answers_en.append(text_en)
 
                 answer_record = {
@@ -304,6 +306,7 @@ def get_consultant_answers_export(visit: Visit):
     # Pre-group answers by question_id
     all_answers = list(visit.consultant_answers.all())
     all_answers.sort(key=lambda x: x.created_at, reverse=True)
+    lang = visit.case.language
     answers_map = {}
     for ans in all_answers:
         if ans.question_id not in answers_map:  # type: ignore
@@ -321,24 +324,24 @@ def get_consultant_answers_export(visit: Visit):
             }
         else:
             # Use prefetched options from the question
-            options = {opt.code: opt for opt in question.options.all()}  # type: ignore
+            options: dict[str, ClientOption] = {opt.code: opt for opt in question.options.all()}  # type: ignore
 
             answers_en = []
             for code, text in zip(answer.choices, answer.texts):
                 option = options.get(code)
-                if not option:
+                if option is None:
                     text_en = text
-                elif option.choices_en:
+                elif option.choices_en: # type: ignore
                     original_choices = getattr(option, f"choices_{lang}", [])
                     try:
                         idx = original_choices.index(code)
-                        text_en = option.choices_en[idx]
+                        text_en = option.choices_en[idx] # type: ignore
                     except ValueError:
                         text_en = text
                 elif option.allow_text:
-                    text_en = option.text_en + ": " + text
+                    text_en = option.text_en + ": " + text # type: ignore
                 else:
-                    text_en = option.text_en if option.text_en else text
+                    text_en = option.text_en if option.text_en else text # type: ignore
                 answers_en.append(text_en)
 
             answer_record = {
